c44452fb96e1d39872cc7d08717be16d0b70b9e3
KARTHIK MOGAVEER <karthikmogaveer206@gmail.com>
2024-12-27 11:18:25 +0530

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main() {
+    char opt;
+    int a,b;
+    printf("chooe (+,-,/,*) operation to perform calculaion");
+    scanf("%c",&opt);
+    if (opt=='+')
+    {
+        printf("you choosed addition\n");
+    }
+    else if (opt=='-')
+    {
+        printf("you choosed substration\n");
+    }
+    else if (opt=='*')
+    {
+         printf("you choosed multiplication\n");
+    }
+    else if (opt=='/')
+    {
+        printf("you choosed divisoon\n");
+    }
+    printf("enter first operands to perform calculation\n");
+    scanf("%d",&a);
+    printf("enter seconad operands to perform calculation\n");
+    scanf("%d",&b);
+    switch (opt)
+    {
+     case '+':
+        printf("%d + %d = %d",a,b,a+b);
+        break;
+     case '-':
+        printf("%d - %d = %d",a,b,a-b);
+        break;
+     case '*':
+        printf("%d * %d = %d",a,b,a*b);
+        break;
+     case '/':
+        printf("%d / %d = %d",a,b,a/b);
+        break;
+    default:
+        printf("you typed a wrong operatot for calculation");
+    }
+return 0;
+}
+
